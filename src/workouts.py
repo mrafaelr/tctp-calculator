064d3f27b76630dd32dfc83ef46b143235ea2ce6
mrafaelr <rafaelmontro1996@gmail.com>
2025-07-26 20:25:28 -0300

    fix: better structure for CTS test

--- src/workouts.py
+++ src/workouts.py
@@ -12,20 +12,18 @@ Spin ups
 - 1' @ Z4
 - 3' @ Z1
 - 1' @ Z5
-- 3' @ Z1
-- 1' @ Z5
 
 Recovery
 - 5' @ Z1
 
-First test Interval
-- 8'
+First test interval
+- First test interval 8' @ Z4-Z5
 
 Recovery
 - 10' @ Z1
 
-Second test Interval
-- 8'
+Second test interval
+- Second test interval 8' @ Z4-Z5
 
 Cool down
 - 10' @ Z1
